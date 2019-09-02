2aaee2825f32c9e152e839f93f439f733cd0c8cf
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-09-02 12:41:48 -0700

    Added 2.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 """
     barcodeapi
 
-    Barcode APIs let you generate barcode images, and recognize values from images of barcodes.  # noqa: E501
+    
 
     OpenAPI spec version: v1
     
@@ -14,13 +14,16 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "cloudmersive_barcode_api_client"
-VERSION = "2.0.1"
+VERSION = "2.0.2"
 # To install the library, run the following
 #
 # python setup.py install
 #
 # prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 REQUIRES = [
     "certifi>=2017.4.17",
@@ -35,12 +38,13 @@ setup(
     version=VERSION,
     description="barcodeapi",
     author_email="",
-    url="",
+    url="https://www.cloudmersive.com/barcode-api",
     keywords=["Swagger", "barcodeapi"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
-    long_description="""\
-    Barcode APIs let you generate barcode images, and recognize values from images of barcodes.  # noqa: E501
-    """
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    
+
 )
